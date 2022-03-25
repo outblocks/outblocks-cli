d57ca631c28d5d073429148882a2cfb259fe64f1
Robert Kopaczewski <rk@23doors.com>
2022-03-25 16:27:40 +0100

    hotfix: complex type resolve

--- pkg/config/vars.go
+++ pkg/config/vars.go
@@ -26,25 +26,33 @@ func isPathInMap(path []string, m map[string]bool) bool {
 }
 
 func expandYAMLString(n *ast.StringNode, file string, vars map[string]interface{}, essentialKeys map[string]bool, path []string) (ast.Node, error) {
-	tknCount := strings.Count(n.String(), "${")
+	tknCount := strings.Count(n.Value, "${")
 	if tknCount == 0 {
 		return n, nil
 	}
+
+	complexType := false
 
 	output, _, err := plugin_util.NewBaseVarEvaluator(vars).
 		WithEncoder(func(c *plugin_util.VarContext, val interface{}) ([]byte, error) {
 			t := reflect.TypeOf(val)
 
-			if t.Kind() == reflect.Slice || t.Kind() == reflect.Map {
+			switch {
+			case t.Kind() == reflect.Slice || t.Kind() == reflect.Map:
 				if len(c.Input) != (c.TokenColumnEnd - c.TokenColumnStart + 1) {
 					return nil, fmt.Errorf("to substitute non-primitive value, it cannot be a part of a string and needs to be unquoted")
 				}
+
+				complexType = true
+
+			case t.Kind() == reflect.String:
+				return []byte(val.(string)), nil
 			}
 
 			return json.Marshal(val)
 		}).
 		WithSkipRowColumnInfo(true).
-		WithVarChar('$').ExpandRaw([]byte(n.String()))
+		WithVarChar('$').ExpandRaw([]byte(n.Value))
 	if err != nil {
 		if essentialKeys == nil || isPathInMap(path, essentialKeys) {
 			var pp printer.Printer
@@ -54,6 +62,10 @@ func expandYAMLString(n *ast.StringNode, file string, vars map[string]interface{
 		}
 
 		return n, nil
+	}
+
+	if !complexType {
+		output = []byte(fmt.Sprintf("%q", string(output)))
 	}
 
 	tok, err := parser.ParseBytes(output, 0)
