9bce1879875b4007fc63effb767809aaab82be96
Robert Kopaczewski <rk@23doors.com>
2022-04-04 20:01:21 +0200

    alpha: allow running docker as root through flag

--- dockerfiles/entrypoint.sh
+++ dockerfiles/entrypoint.sh
@@ -6,4 +6,8 @@ PGID=${PGID:-1000}
 groupmod -o -g "${PGID}" outblocks > /dev/null
 usermod -o -u "${PUID}" outblocks > /dev/null
 
-exec gosu outblocks "$@"
+if [ "${RUN_AS_ROOT:-0}" = "1" ]; then
+    exec "$@"
+else
+    exec gosu outblocks "$@"
+fi
